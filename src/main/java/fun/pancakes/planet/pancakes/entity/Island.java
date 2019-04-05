e7f15c920e706864e91804fd093dfa703b8b7e62
alex-stewart <imalexstewart+git@gmail.com>
2019-04-05 19:27:00 +0100

    Revert "Migrating to dynamodb"
    This reverts commit e60c95c

--- src/main/java/fun/pancakes/planet/pancakes/entity/Island.java
+++ src/main/java/fun/pancakes/planet/pancakes/entity/Island.java
@@ -1,13 +1,13 @@
 package fun.pancakes.planet.pancakes.entity;
 
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Builder
-@DynamoDBTable(tableName = "island")
+@Document(collection = "islands")
 public class Island {
 
     @Id
