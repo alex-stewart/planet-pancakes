e19be8d20690a9db6d2cab0a0e7cdc340773fd60
alex-stewart <imalexstewart+git@gmail.com>
2019-08-29 22:45:00 +0100

    Removed unused import

--- src/main/java/fun/pancakes/planet_pancakes/config/ApplicationConfig.java
+++ src/main/java/fun/pancakes/planet_pancakes/config/ApplicationConfig.java
@@ -3,7 +3,6 @@ package fun.pancakes.planet_pancakes.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 public class ApplicationConfig {
