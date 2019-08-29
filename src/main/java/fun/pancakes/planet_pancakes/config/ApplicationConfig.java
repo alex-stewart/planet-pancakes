edde1f9d25ae45974d2a8e3b9b7f49face71815d
alex-stewart <imalexstewart+git@gmail.com>
2019-08-29 22:44:46 +0100

    Discord login

--- src/main/java/fun/pancakes/planet_pancakes/config/ApplicationConfig.java
+++ src/main/java/fun/pancakes/planet_pancakes/config/ApplicationConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:secret.properties")
 public class ApplicationConfig {
 
     @Bean
