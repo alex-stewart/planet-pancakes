74b661de4866ea2f3b9b5455a6f97b340f4cafc3
alex-stewart <imalexstewart+git@gmail.com>
2019-12-08 21:01:22 +0000

    Fixing missing alt on image

--- src/main/webapp/WEB-INF/view/react/src/components/NewsPaper.js
+++ src/main/webapp/WEB-INF/view/react/src/components/NewsPaper.js
@@ -5,29 +5,33 @@ import DateAndTime from "./DateAndTime";
 export default class NewsPaper extends Component {
 
     render() {
-        let newspaperTitle = this.props.paper.newspaperTitle;
+        let paper = this.props.paper;
+        
+        let newspaperTitle = paper.newspaperTitle;
         let divierStyle = {
-            "background-color": this.props.paper.newspaperColour
+            "background-color": paper.newspaperColour
         };
 
-        let headline = this.props.paper.headline;
-        let headlineStory = this.props.paper.headlineStory;
-        let imageUrl = this.props.paper.imageUrl;
-        let secondaryHeadlines = this.props.paper.secondaryHeadlines || [];
+        let headline = paper.headline;
+        let headlineStory = paper.headlineStory;
+        let imageUrl = paper.imageUrl;
+        let secondaryHeadlines = paper.secondaryHeadlines || [];
 
         return <div className={"newspaper-body container"}>
             <div className={"display-1 text-center"}>
                 {newspaperTitle}
             </div>
             <div className={"newspaper-divider text-center"} style={divierStyle}>
-                <DateAndTime days={this.props.paper.publishDay}/>
+                <DateAndTime days={paper.publishDay}/>
             </div>
             <div className={"display-1 text-center newspaper-headline"}>
                 {headline}
             </div>
             <Row>
                 <Col>
-                    <img className={"newspaper-image"} src={imageUrl}/>
+                    <img className={"newspaper-image"}
+                         src={imageUrl}
+                         alt={"newspaper-image-" + paper.id}/>
                 </Col>
                 <Col>
                     {headlineStory}
