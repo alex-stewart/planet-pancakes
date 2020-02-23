2ec162f438cae87a84b44b53918bd56fea3bf47e
alex-stewart <imalexstewart+git@gmail.com>
2020-02-23 13:38:23 +0000

    Moved newspaper styling into the component

--- src/main/webapp/WEB-INF/view/react/src/components/news/NewsPaper.js
+++ src/main/webapp/WEB-INF/view/react/src/components/news/NewsPaper.js
@@ -1,6 +1,32 @@
 import React, {Component} from 'react';
 import {Col, Row} from 'reactstrap';
 import {GameDate} from "../../util/GameDate";
+
+const secondaryHeadlineStyle = {
+    marginTop: '20px'
+};
+
+const newspaperImageStyle = {
+    display: 'block',
+    margin: 'auto',
+    objectFit: 'cover',
+    width: '100%',
+    height: '100%'
+};
+
+const newspaperHeadlineStyle = {
+    textAlign: 'justify'
+};
+
+const newspaperBodyStyle = {
+    backgroundColor: '#FFFFFF',
+    padding: '20px',
+    marginBottom: '100px',
+    whiteSpace: 'pre-line',
+    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+    fontFamily: '"Times New Roman", Times, serif'
+};
+
 export default class NewsPaper extends Component {
 
     render() {
@@ -8,7 +34,9 @@ export default class NewsPaper extends Component {
         
         let newspaperTitle = paper.newspaperTitle;
         let dividerStyle = {
-            'backgroundColor': paper.newspaperColour
+            width: '100%',
+            color: '#FFFFFF',
+            backgroundColor: paper.newspaperColour
         };
 
         let headline = paper.headline;
@@ -17,20 +45,22 @@ export default class NewsPaper extends Component {
         let secondaryHeadlines = paper.secondaryHeadlines || [];
         let date = GameDate.fromTimestamp(paper.publishDay);
 
-        return <div className={"newspaper-body container"}>
+        return <div style={newspaperBodyStyle}
+                    className={"container"}>
             <div className={"display-1 text-center"}>
                 {newspaperTitle}
             </div>
-            <div className={"newspaper-divider text-center"}
+            <div className={"text-center"}
                  style={dividerStyle}>
                 {date.toStringWithoutTime()}
             </div>
-            <div className={"display-1 text-center newspaper-headline"}>
+            <div style={newspaperHeadlineStyle}
+                 className={"display-1 text-center"}>
                 {headline}
             </div>
             <Row>
                 <Col>
-                    <img className={"newspaper-image"}
+                    <img style={newspaperImageStyle}
                          src={imageUrl}
                          alt={"newspaper-image-" + paper.id}/>
                 </Col>
@@ -38,7 +68,8 @@ export default class NewsPaper extends Component {
                     {headlineStory}
                 </Col>
             </Row>
-            <div className={"newspaper-secondary-headlines h4"}>
+            <div style={secondaryHeadlineStyle}
+                 className={"h4"}>
                 {secondaryHeadlines.map(function (headline) {
                     return <span key={headline}>○ {headline} </span>
                 }, this)} ○
