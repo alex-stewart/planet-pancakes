f896c044f2fd7ddaa0ebcd435a856097cdfe124e
alex-stewart <imalexstewart+git@gmail.com>
2020-03-01 13:17:11 +0000

    Updated town and city icons

--- src/main/webapp/WEB-INF/view/react/src/components/map/icon/SettlementIcons.js
+++ src/main/webapp/WEB-INF/view/react/src/components/map/icon/SettlementIcons.js
@@ -1,18 +1,18 @@
 import L from 'leaflet';
 
 const cityIcon = new L.Icon({
-    iconSize: new L.Point(20, 20),
-    iconUrl: require('./img/city.png'),
-    iconRetinaUrl: require('./img/city.png'),
+    iconSize: new L.Point(40, 40),
+    iconUrl: require('./img/city.svg'),
+    iconRetinaUrl: require('./img/city.svg'),
     shadowUrl: null,
     shadowSize: null,
     shadowAnchor: null,
 });
 
 const townIcon = new L.Icon({
-    iconSize: new L.Point(20, 20),
-    iconUrl: require('./img/town.png'),
-    iconRetinaUrl: require('./img/town.png'),
+    iconSize: new L.Point(40, 40),
+    iconUrl: require('./img/town.svg'),
+    iconRetinaUrl: require('./img/town.svg'),
     shadowUrl: null,
     shadowSize: null,
     shadowAnchor: null,
