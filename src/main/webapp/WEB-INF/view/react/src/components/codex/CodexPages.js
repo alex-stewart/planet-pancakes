7a07a299acd551d6144891f11df98a66ad0bb341
alex-stewart <imalexstewart+git@gmail.com>
2020-03-14 10:31:59 +0000

    Updated coins, and added hpe ring codex page

--- src/main/webapp/WEB-INF/view/react/src/components/codex/CodexPages.js
+++ src/main/webapp/WEB-INF/view/react/src/components/codex/CodexPages.js
@@ -1,5 +1,9 @@
 export const CODEX_PAGES = [
     {
+        name: ' Holy Purple Expansionist Pantheon',
+        image: 'hpe-ring.png'
+    },
+    {
         name: 'Language Tree',
         image: 'language-tree.png'
     },
