520a38bf86a068706a6a12d9950285d07c36f156
alex-stewart <imalexstewart+git@gmail.com>
2020-04-10 17:30:39 +0100

    Setting size of the herald canvas

--- src/main/webapp/WEB-INF/view/react/src/components/herald/Herald.js
+++ src/main/webapp/WEB-INF/view/react/src/components/herald/Herald.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Helmet} from "react-helmet";
+import {Button, ButtonGroup} from "reactstrap";
 
 export const heraldPageStyle = {
     backgroundColor: '#F8ECC2',
@@ -16,9 +17,29 @@ export default class Herald extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedCodexPage: null
-        }
+            typeDropdownOpen: false,
+            selectedSize: 1
+        };
     }
+
+    componentDidMount() {
+        let ctx = this.refs.canvas.getContext('2d');
+        this.setState({
+            canvasContext: ctx
+        });
+        this.draw(ctx, 1)
+    }
+
+    draw(context, size) {
+        context.fillStyle= "#FFFFFF";
+        context.clearRect(0, 0, this.refs.canvas.width, this.refs.canvas.height);
+        context.fillRect(0, 0, size * 100, size * 100);
+    }
+
+    setSize(size) {
+        this.setState({selectedSize: size});
+        this.draw(this.state.canvasContext, size);
+    };
 
     render() {
         return (
@@ -26,6 +47,23 @@ export default class Herald extends Component {
                 <Helmet>
                     <title>{"PP - Herald"}</title>
                 </Helmet>
+                <div>
+                    <ButtonGroup color="dark">
+                        <Button color="dark"
+                                onClick={() => this.setSize(1)}>
+                            Small
+                        </Button>
+                        <Button color="dark"
+                                onClick={() => this.setSize(2)}>
+                            Medium
+                        </Button>
+                        <Button color="dark"
+                                onClick={() => this.setSize(3)}>
+                            Large
+                        </Button>
+                    </ButtonGroup>
+                </div>
+                <canvas ref={"canvas"} width={1000} height={1000}/>
             </div>
         )
     }
